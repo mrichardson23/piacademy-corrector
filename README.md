cb598255241d3b8e4051348e9624f85f37a82c89
Matt Richardson <mrichardson23@gmail.com>
2018-07-05 13:58:48 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Piacademy Corrector
+
+This project uses the Twython API to look for tweets with the #Piacademy hashtag and replies with the correct #Picademy hashtag.
---
+++ bot.py
@@ -0,0 +1,44 @@
+from twython import Twython
+from twython import TwythonStreamer
+from random import choice
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+
+
+messages = [
+	" Hello there! The correct hashtag is #Picademy.",
+	" What a great tweet! Use the hashtag #Picademy instead.",
+	" I want to make sure your fellow educators see your tweets, so be sure to use the hashtag #Picademy instead.",
+	" I love your enthusiasm. Join the conversation with the hashtag #Picademy (note the spelling).",
+	" You've use the wrong hashtag... and that is OK. But please use #Picademy going forward.  Thanks.",
+	" It seems you've got an extra 'a' in your tweet.  The correct hashtag is #Picademy"
+]
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+         if 'text' in data:
+             print("Received a tweet: " + data['text'])
+             twitter.update_status(status="@" + data['user']['screen_name'] + choice(messages), in_reply_to_status_id=data['id'])
+             
+         
+stream = MyStreamer(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+
+stream.statuses.filter(track='#piacademy')
+         
